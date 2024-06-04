8ee369c022ed4690cadf5bc72a53d6d7620e60d1
Quentin-Dubois <70204656+Quentin-Dubois@users.noreply.github.com>
2024-06-04 09:39:17 +0200

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,76 +2,54 @@ window.onbeforeprint = beforePrint;
 
 let modeAsso = "AMSEAA";
 
-document.getElementById("nomInput").addEventListener("change", (e) => {
-    document.getElementById("nomOutput").innerText = e.target.value;
-    nomToIdentifiantInput(e);
-    document.getElementsByTagName("title").item(0).innerText = e.target.value;
-    }
-);
-
-document.getElementById("identifiantInput").addEventListener("change", (e) => 
-    Array.from(document.getElementsByClassName("identifiantOutput")).forEach((element) => {
-        inputToOutput(element, e)
-    })
-);
-
-document.getElementById("identifiantInput").addEventListener("change", (e) => 
-    Array.from(document.getElementsByClassName("identifiantOutput2")).forEach((element) => {
-        inputToOutput(element, e)
-    })
-);
-
-document.getElementById("passwordInput").addEventListener("change", (e) => 
+document.getElementById("prenomInput").addEventListener("input", updateNames);
+document.getElementById("nomInput").addEventListener("input", updateNames);
+document.getElementById("passwordInput").addEventListener("input", (e) => 
     Array.from(document.getElementsByClassName("passwordOutput")).forEach((element) => {
-        inputToOutput(element, e)
+        inputToOutput(element, e);
     })
 );
 
-function inputToOutput(element, e){
-    element.innerText = e.target.value
+function updateNames() {
+    let prenom = document.getElementById("prenomInput").value;
+    let nom = document.getElementById("nomInput").value;
+    document.getElementById("nomOutput").innerText = prenom + " " + nom;
+    document.getElementsByTagName("title").item(0).innerText = prenom + " " + nom;
+
+    let identifiant1 = (prenom.charAt(0) + nom).toLowerCase();
+    document.getElementById("identifiantOutput").innerText = identifiant1;
+
+    let identifiant2 = `${prenom.toLowerCase()}.${nom.toLowerCase()}`;
+    document.getElementById("identifiantOutput2").innerText = identifiant2;
 }
 
-function nomToIdentifiantInput(eNom){
-    let stringId = (eNom.target.value.split(" ")[0].charAt(0) + eNom.target.value.split(" ")[1]).toLowerCase();
-    let identifiantInput = document.getElementById("identifiantInput");
-    identifiantInput.value = stringId;
-    identifiantInput.dispatchEvent(new Event("change"));
+function inputToOutput(element, e) {
+    element.innerText = e.target.value;
 }
 
-/*prenom.nom@amseaa.fr*/
-function nomToIdentifiantInput2(eNom) {
-    let noms = eNom.target.value.split(" ");
-    let mot1 = noms[0].toLowerCase();
-    let mot2 = noms[1].toLowerCase();
-    let stringId = mot1 + "." + mot2;
-    let identifiantInput2 = document.getElementById("identifiantInput");
-    identifiantInput.value = stringId;
-    identifiantInput.dispatchEvent(new Event("change"));
-}
-
-function beforePrint(){
+function beforePrint() {
     let date = new Date();
     let formattedDate = date.getDate() + " " + mois[date.getMonth()] + " " + date.getFullYear();
     document.getElementById("dateOutput").innerText = formattedDate;
 }
 
-function changeLogo(){
+function changeLogo() {
     let logoPage = document.getElementById("logoPage");
     let vague = document.getElementById("vague");
-    if(modeAsso === "AMSEAA"){
+    if (modeAsso === "AMSEAA") {
         document.getElementById("titrePage").innerText = "Comptes Informatiques AMSEAA";
-        logoPage.src ="assets/img/logo_amseaa.jpg";
-        vague.src ="assets/img/vague_amseaa.jpg";
-        document.getElementById("changeLogoBtn").innerText = "AMSEAA"
+        logoPage.src = "assets/img/logo_amseaa.jpg";
+        vague.src = "assets/img/vague_amseaa.jpg";
+        document.getElementById("changeLogoBtn").innerText = "AMSEAA";
         modeAsso = "AMSEAA";
     } else {
         document.getElementById("titrePage").innerText = "Comptes Informatiques AMSEAA";
-        logoPage.src ="assets/img/logo_amseaa.jpg";
-        logoPage.crossOrigin ="anonymous";
-        vague.src ="assets/img/vague_amseaa.jpg";
-        document.getElementById("changeLogoBtn").innerText = "AMSEAA"
-        modeAsso = "AMSEAA"; 
+        logoPage.src = "assets/img/logo_amseaa.jpg";
+        logoPage.crossOrigin = "anonymous";
+        vague.src = "assets/img/vague_amseaa.jpg";
+        document.getElementById("changeLogoBtn").innerText = "AMSEAA";
+        modeAsso = "AMSEAA";
     }
 }
 
-var mois = new Array( "Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre" );
+var mois = new Array("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre");
